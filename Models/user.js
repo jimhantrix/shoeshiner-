d20658effbc7909c71b4296986d23a4ddc4681cc
jimhantrix <jimtekanyo@gmail.com>
2020-04-11 17:19:22 +0800

    created a user model, updated event model

---
+++ Models/user.js
@@ -0,0 +1,22 @@
+const mongoose = required('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+
+email: {
+  type: String,
+  required: true
+},
+password: {
+  type: String,
+  required: true
+},
+CreatedEvents: [{
+  type: Schema.Types.ObjectId,
+  ref: 'Event'
+    },
+  ]
+});
+
+module.exports = mongoose.model('User', userSchema);
