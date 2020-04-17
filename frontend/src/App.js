b4f7e4735ca6d5c7c0131c22e32a2b4a7681e2af
jimhantrix <jimtekanyo@gmail.com>
2020-04-17 20:22:46 +0800

    Updated front end pages file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import AuthPage from './pages/Auth';
+import BookingsPage from './pages/Bookings';
+import EventsPage from './pages/Events';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    <h1>Shoeshiner</h1>
-    </div>
+    <BrowserRouter>
+    <Switch>
+    <Redirect from = "/" to ="/auth" exact/>
+    <Route path = "/auth" component ={AuthPage}/>
+    <Route path = "/events" component ={EventsPage}/>
+    <Route path = "/bookings" component ={BookingsPage}/>
+    </Switch>
+    </BrowserRouter>
   );
 }
 
